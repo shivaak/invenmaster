02c4d3c7014a8bf9064b054903480e11f7474b74
Shivaa Krishnan <shivaak@uber.com>
2024-10-07 22:57:32 +0530

    Test 7

--- src/main/java/com/application/invenmaster/controller/HelloController.java
+++ src/main/java/com/application/invenmaster/controller/HelloController.java
@@ -17,7 +17,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String getHello(){
-        return "Muruga from test6 " + applicationInfo.getAppName();
+        return "Muruga from test7 " + applicationInfo.getAppName();
     }
 
     @GetMapping("version")
